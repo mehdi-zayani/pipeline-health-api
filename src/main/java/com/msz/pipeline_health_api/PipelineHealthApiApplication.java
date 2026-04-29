87e9d99c7601a765be5e1efa946ab9293906819c
Mehdi Zayani <mehdi.zayani.dev@gmail.com>
2026-04-29 22:38:19 +0100

    init: bootstrap Spring Boot project with H2 and base configuration

---
+++ src/main/java/com/msz/pipeline_health_api/PipelineHealthApiApplication.java
@@ -0,0 +1,13 @@
+package com.msz.pipeline_health_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineHealthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineHealthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msz/pipeline_health_api/PipelineHealthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msz.pipeline_health_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineHealthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
